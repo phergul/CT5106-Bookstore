6e2351008ede8fad19c697687a3bd38ef4fa3a0d
TripO7 <49369798+TripO7@users.noreply.github.com>
2024-10-06 22:38:53 +0100

    Review Repo JPA
    doesnt work

--- src/main/java/com/ct5106/bookstore/domain/ReviewRepository.java
+++ src/main/java/com/ct5106/bookstore/domain/ReviewRepository.java
@@ -1,10 +1,20 @@
 package com.ct5106.bookstore.domain;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ReviewRepository  extends CrudRepository<Review, Long>
 {
+	 // Query method using keywords and property names
+    List<Review> findDistinctByRatingAndReviewDate(float rating, LocalDate reviewDate);
 
+    // Query method using JPQL and @Query annotation
+    @Query("SELECT r FROM Review r WHERE r.rating > :rating")
+    List<Review> findReviewsWithRatingGreaterThan(@Param("rating") float rating);
 }
